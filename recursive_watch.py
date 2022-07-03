293ce0e9c14d02879729345ed89dca3ecee52689
Orel Damari <orel@mip.me>
2022-07-03 06:54:31 +0300

    progresss

--- recursive_watch.py
+++ recursive_watch.py
@@ -2,15 +2,18 @@ import inotify.adapters
 
 
 def _main():
-    i = inotify.adapters.InotifyTree('/tmp')
+    i = inotify.adapters.InotifyTree('/')
 
     for event in i.event_gen():
-        if event != None:
-            (_, type_names, path, filename) = event
+        try:
+            if event != None:
+                (_, type_names, path, filename) = event
 
-            print("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format(
-                path, filename, type_names))
-        pass
+                print("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format(
+                    path, filename, type_names))
+            pass
+        except:
+            pass
 
 
 if __name__ == '__main__':
