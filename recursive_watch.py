1ab6dbc4b56d1c4b41c2fa9077fa4d74e82e4086
Orel Damari <orel@mip.me>
2022-07-03 06:06:12 +0300

    progrees

--- recursive_watch.py
+++ recursive_watch.py
@@ -1,15 +1,17 @@
 import inotify.adapters
 
+
 def _main():
     i = inotify.adapters.InotifyTree('/tmp')
 
     for event in i.event_gen():
-        print(f"A FILE HAVE CHANGED {event}")
-        # (_, type_names, path, filename) = event
-        #
-        # print("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format(
-        #     path, filename, type_names))
+        if event != None:
+            (_, type_names, path, filename) = event
+
+            print("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format(
+                path, filename, type_names))
         pass
+
 
 if __name__ == '__main__':
     _main()
